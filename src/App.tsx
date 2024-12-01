82c96aeddc1356e1212b0a5c755327ba7eb1aff4
brian Liu <bliu9881@gmail.com>
2024-11-30 16:49:26 -0800

    add top and side bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,31 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { Routes, Route } from "react-router-dom";
 import { useAuthenticator } from "@aws-amplify/ui-react";
-
-const client = generateClient<Schema>();
+import NavBar from "./components/NavBar";
+import Sidebar from "./components/Sidebar";
+import TodoPage from "./pages/TodoPage";
+import AboutPage from "./pages/AboutPage";
+import TutorialsPage from "./pages/TutorialsPage";
+import KnowledgeBasePage from "./pages/KnowledgeBasePage";
 
 function App() {
-  const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  const { user } = useAuthenticator();
 
   return (
-    <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
-            {todo.content}
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+    <>
+      <NavBar />
+      <div className="layout">
+        <Sidebar />
+        <main>
+          <Routes>
+            <Route path="/" element={<TodoPage />} />
+            <Route path="/todos" element={<TodoPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/tutorials" element={<TutorialsPage />} />
+            <Route path="/knowledge-base" element={<KnowledgeBasePage />} />
+          </Routes>
+        </main>
       </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+    </>
   );
 }
 
